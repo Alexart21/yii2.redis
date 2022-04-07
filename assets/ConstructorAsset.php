32aee2348d84af22639599aff50d087fc148af99
Александр Михайлов <iskander.m.211@gmail.com>
2022-04-07 17:30:24 +0300

    next

--- assets/ConstructorAsset.php
+++ assets/ConstructorAsset.php
@@ -9,11 +9,11 @@ class ConstructorAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'vue_assets/css/app.c3441e6c.css',
+        'vue_assets/css/app.fec2e6d7.css',
     ];
     public $js = [
         'vue_assets/js/chunk-vendors.177daa20.js',
-        'vue_assets/js/app.43df74c9.js',
+        'vue_assets/js/app.0938ddd4.js',
     ];
     public $depends = [
     ];
