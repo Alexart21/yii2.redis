4cecb748253a76b21354458e05fe68e86bcc26ff
Александр Михайлов <iskander.m.211@gmail.com>
2022-04-08 19:58:49 +0300

    next

--- views/constructor/index.php
+++ views/constructor/index.php
@@ -2,7 +2,19 @@
 $this->title = 'Онлайн дизайн чехлов';
 ?>
 <noscript><strong>Для отображения страницы включите  Javascript !</strong></noscript>
-<div id="app">Загрузка...</div>
+<style>@keyframes dash {
+           to {
+               stroke-dashoffset: 136;
+           }
+       }
+    @keyframes rotate {
+        100% {
+            transform: rotate(360deg);
+        }
+    }</style>
+<div id="app">
+    <svg width="100px" height="100px" style="position: absolute;left:calc(50% - 50px);top:calc(50% - 50px)" viewBox="-3 -4 39 39"><polygon style="stroke-dasharray: 17;animation: dash 2.5s cubic-bezier(0.35, 0.04, 0.63, 0.95) infinite;" fill="#ffffff" stroke="#ff0000" stroke-width="1" points="16,0 32,32 0,32"></polygon></svg>
+</div>
 <script>window.onload = () => {
         Coloris({
             el: '.coloris',
